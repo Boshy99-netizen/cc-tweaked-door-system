2d504ebf701c5447b28a462c2c4dc14641ce1ce1
Boshy99-netizen <sevko2281337@gmail.com>
2026-05-03 21:21:01 +0200

    guest key.lua

---
+++ guest key.lua
@@ -0,0 +1,39 @@
+local INTERVAL = 1
+local KEY_CHANNEL = 100
+
+local playerName = settings.get("key_owner")
+if not playerName then
+    print("Enter your Minecraft name:")
+    playerName = read()
+    settings.set("key_owner", playerName)
+    settings.save()
+end
+
+print("=== GUEST KEY ===")
+print("Player: " .. playerName)
+
+local modem = peripheral.find("modem")
+if not modem then
+    print("ERROR: No wireless modem!")
+    return
+end
+
+local modemName = peripheral.getName(modem)
+modem.open(KEY_CHANNEL + 1)
+
+while true do
+    modem.transmit(KEY_CHANNEL, KEY_CHANNEL + 1, {
+        type = "KEY_PING",
+        keyType = "guest",
+        player = playerName,
+        timestamp = os.time()
+    })
+    
+    term.setCursorPos(1, 4)
+    term.clearLine()
+    term.setTextColor(colors.lightBlue)
+    term.write("[GUEST] " .. os.time())
+    term.setTextColor(colors.white)
+    
+    sleep(INTERVAL)
+end
